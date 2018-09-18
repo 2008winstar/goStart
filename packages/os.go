05610605e0865b466c6f95af4e2cd9a5e1bbd51f
2008winstar <2008winstar@sina.com>
2018-09-18 20:34:56 +0800

    add demo

---
+++ packages/os.go
@@ -0,0 +1,12 @@
+package packages
+
+/*
+ * os
+ * func Open(fileName string) (fileHandler, error)
+ * 	fileHandler.Close()
+ * 	fileHandler.Stat()
+ * 	fileHandler.Read()
+
+ * func Create(fileName string) (fileHandler, error)
+ * fileHandler.WriteString(s string)
+ */
---
+++ packages/strings.go
@@ -0,0 +1,16 @@
+package packages
+
+/*
+ * strings
+ * 	func Contains(s, subStr string) bool
+ * 	func Count(s, sep string) bool
+ *	func HasPrefix(s, prefix string) bool
+ *	func HasSuffix(s, suffix string) bool
+ *	func Index(s, sep string) int
+ *	func Join(a []string, sep string) string
+ *	func Repeat(s string, count int) string
+ *	func Replace(s, old, new string, n int) string
+ *	func Split(s, sep string) []string
+ *	func ToLower(s string) string
+ *	func ToUpper(s string) string
+ */
